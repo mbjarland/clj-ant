dce13b4f89a67c9f4c1ebe63ca046ff9430e9c74
Matias Bjarland <matias@iteego.com>
2018-03-12 21:09:22 +0100

    working on documentation etc
    Expands the surrounding documentation and cleanup work while the generated API
    surface takes shape.

--- src/clj/clj_ant/core.clj
+++ src/clj/clj_ant/core.clj
@@ -36,31 +36,7 @@
 ;    return project;
 ;}
 
-(defn invoke [task method args]
-  (clojure.lang.Reflector/invokeInstanceMethod
-    task
-    method
-    (to-array args)))
-
-(defn create-project []
-  (let [p  (Project.)
-        ph (ProjectHelper/getProjectHelper)
-        bl (NoBannerLogger.)]
-    (.addReference p ProjectHelper/PROJECTHELPER_REFERENCE ph)
-    (.addElement (.getImportStack ph) "clj-ant")
-    (.setMessageOutputLevel bl Project/MSG_INFO)
-    (.setOutputPrintStream bl System/out)
-    (.setErrorPrintStream bl System/err)
-    (.init p)
-    (.getBaseDir p)
-    p))
-
-(def create-project-m (memoize create-project))
-
-(defn display-name [o]
-  (.getSimpleName (class o)))
-
-(defn partition-args [args]
+(defn- partition-args [args]
   (reduce
     (fn [[a n] [k v]]
       (cond
@@ -70,101 +46,14 @@
     [{} []]
     (partition-all 2 args)))
 
-(defn parse-args [name args]
+(defn- ant-xml [name args]
   (let [t (Throwable.)
         [attrs nested] (partition-args args)]
     (xml/element name
                  (merge attrs {:trace (fn [] t)})
                  nested)))
 
-(defn method->dsl-kw [m pre-len]
-  (keyword (.toLowerCase (.substring (name (:name m)) pre-len))))
-
-(defn accessors [clazz]
-  (let [s (:members (reflect/type-reflect clazz :ancestors true))]
-    (reduce
-      (fn [a m]
-        (let [m (dissoc m :return-type :exception-types :declaring-class :flags)]
-          (condp #(str/starts-with? %2 %1) (name (:name m))
-            "add" (assoc-in a [:adders (count (:adders a))] m)
-            "set" (assoc-in a [:setters (method->dsl-kw m 3)] m)
-            "create" (assoc-in a [:creators (method->dsl-kw m 6)] m)
-            a)))
-      {:adders []}
-      (filter #((:flags %) :public) s))))
-
-(def accessors-m (memoize accessors))
-
-(defmulti coerce-type (fn [to-type value] [to-type (class value)]))
-(defmethod coerce-type [File String] [_ value] (io/as-file value))
-(defmethod coerce-type :default [_ value] value)
-
-(defn set-attribute [acs task attr-name value]
-  ;(println "setting attribute" attr-name "on" (display-name task) "to" value)
-  (let [setter      (attr-name (:setters acs))
-        setter-name (str (:name setter))
-        param-type  (resolve (first (:parameter-types setter)))] ;;todo multi param
-    (invoke task setter-name (to-array [(coerce-type param-type value)]))))
-
-(defn add-nested-create [acs task nested]
-  ;(println "adding nested create" (str \' (:name nested) \') "on" (display-name task))
-  (let [dsl-name    (keyword (:name nested))
-        method-name (-> acs :creators dsl-name :name str)
-        created     (invoke task method-name [])
-        [attrs _] (parse-args (:args nested))
-        created-acs (accessors-m (class created))]
-    ;; and now set attrs on the created instance
-    (doseq [[attr-name value] attrs]
-      (set-attribute created-acs created attr-name value))
-    ;;TODO: nested
-    ))                                                      ;;assume no params...TODO: better
-
-(defn add-nested-type [acs task nested]
-  ;(println "adding nested type" (display-name nested) "on" (display-name task))
-  (let [adders       (:adders acs)
-        nested-class (class nested)
-        adder        (first                                 ;;if we find more than one we are in trouble
-                       (filter
-                         (fn [a]
-                           (let [types (:parameter-types a)]
-                             (and (= (count types) 1)       ;only consider one-param adders
-                                  (= nested-class (resolve (first types))))))
-                         adders))
-        adder-name   (str (:name adder))]
-    ;    (prn :adder adder :name adder-name :type (class adder-name) :after (str adder-name) nested-clazz)
-    (invoke task adder-name [nested])))
-
-(defn add-nested [acs task nested]
-  (if (instance? Map nested)
-    (add-nested-create acs task nested)
-    (add-nested-type acs task nested)))
-
-(defn prepare-type [clazz args]
-  (let [[attrs nested] (parse-args args)
-        task (.newInstance clazz)
-        acs  (accessors-m clazz)
-        prj  (create-project-m)]
-    (when (get-in acs [:setters :project])
-      (set-attribute acs task :project prj))
-    (doseq [[attr-name value] attrs]
-      (set-attribute acs task attr-name value))
-    (doseq [n nested]
-      (add-nested acs task n))
-    task))
-
-(defn exclude [& args]
-  (parse-args :exclude args))
-
-(defn include [& args]
-  (parse-args :include args))
-
-(defn filterset [& args]
-  (parse-args :filterset args))
-
-(defn fileset [& args]
-  (parse-args :fileset args))
-
-(defn copy
+(defn- copy
   "Example code:
 
     (ant
@@ -184,10 +73,9 @@
 
   https://ant.apache.org/manual/Tasks/copy.html"
   [& args]
-  (parse-args :copy args))
+  (ant-xml :copy args))
 
-
-(defn run-ant [build-file-str ant-args extra-props classloader]
+(defn- run-ant [build-file-str ant-args extra-props classloader]
   (let [bf   (CljAntBuildFile. "clj-ant-build" build-file-str (io/file "."))
         main (CljAntMain. bf)]
     (CljAntProjectHelper/register)
@@ -223,7 +111,7 @@
 
   )
 
-(defn convert-to-xml [ant-args & nested]
+(defn- convert-to-xml [ant-args & nested]
   (let [prj-attrs (select-keys ant-args [:default :name :basedir])]
     ;(xml/emit
     (xml/indent-str
@@ -290,24 +178,14 @@
         xml       (apply convert-to-xml prj-attrs nested)]
     (run-ant xml ant-attrs nil nil)))
 
+
+(load "core_generated")
 ; anyway, scsh is quite complex and does a lot of cool stuff, there's
 ; like a couple of nice ideas you can steal from it
 ; https://github.com/ChaosEternal/guile-scsh
 ; https://gist.github.com/noisesmith/06102f38f14bad40ebe4a04f79f5dfda
 ; https://gist.github.com/noisesmith/684e09a77ca4390f03fd2e53af1d5464
 
-(comment
-
-  (time
-    (copy :todir "/tmp"
-          (fileset :dir "."
-                   (include :name "ant/**/*.sh")
-                   (exclude :name "**/etc/**")
-                   (exclude :name "**/lib/**")
-                   (exclude :name "**/manual/**")
-                   (exclude :name "**/src/**"))))
-
-  )
 
 (comment
   (def REMEMBER "./src/main/org/apache/tools/ant/types/defaults.properties")
