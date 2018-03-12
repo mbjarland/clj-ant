888052e682493851dc9b8c85fedb4a3e2e92ef00
Matias Bjarland <matias@iteego.com>
2018-03-12 21:09:22 +0100

    working on documentation etc

--- project.clj
+++ project.clj
@@ -2,15 +2,19 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.apache.ant/ant "1.10.2"]
                  [org.clojure/data.xml "0.0.8"]]
-  :source-paths      ["src/clj" "target/generated"]
+  :source-paths ["src/clj" "target/generated"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.8" "-source" "1.8"
                   "-Xlint:deprecation" "-Xlint:options"]
   :jvm-opts ["-Xms512m" "-Xmx2g" "-server"]
 
-	:profiles {:dev {:source-paths ["src/clj" "src/build-src" "target/generated"]}}
+  :profiles {:dev {:dependencies [[clj-tagsoup/clj-tagsoup "0.3.0"]
+                                  [com.rpl/specter "1.1.0"]]
+                   :source-paths ["src/clj"
+                                  "src/build-src"
+                                  "target/generated"]}}
   )
